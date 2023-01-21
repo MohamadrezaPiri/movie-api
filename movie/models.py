d2971adcc21a4f1713ec5023bf63488e4ef1e9f7
MohamadrezaPiri <piry.mreza@gmail.com>
2023-01-21 11:13:20 +0330

    create movie model

---
+++ movie/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateTimeField()
+    cast = models.TextField()
+    crew = models.TextField()
+    plot = models.TextField()
+    poster = models.URLField()
+    imdb_rating = models.FloatField()
+    imdb_votes = models.IntegerField()
+    imdb_id = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
