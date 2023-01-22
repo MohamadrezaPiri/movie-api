2a542c653076dd4ea9cad19c8d21a2c04b71986f
MohamadrezaPiri <piry.mreza@gmail.com>
2023-01-22 11:02:52 +0330

    Add Review model

--- movie/models.py
+++ movie/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -16,3 +16,9 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    text = models.TextField()
