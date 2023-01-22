f151d2d0b7211a150b97dd81619e877170597f12
MohamadrezaPiri <piry.mreza@gmail.com>
2023-01-22 10:48:31 +0330

    Add JWT Authentication

--- movie/serializers.py
+++ movie/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from .models import Movie
 
 
 class MoviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['title', 'release_date', 'cast', 'crew', 'plot',
+        fields = ['id', 'title', 'release_date', 'cast', 'crew', 'plot',
                   'poster', 'imdb_rating', 'imdb_votes', 'imdb_id']
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'username', 'first_name',
+                  'last_name', 'email', 'password']
