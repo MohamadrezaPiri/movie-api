a887d84717296a7ef28ba63904d283683531ab83
MohamadrezaPiri <piry.mreza@gmail.com>
2023-01-24 14:29:19 +0330

    Add ratings router

--- movie/urls.py
+++ movie/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 # from rest_framework.routers import SimpleRouter
 from rest_framework_nested.routers import SimpleRouter, NestedSimpleRouter
-from .views import MovieViewSet, import_movies, ReviewViewSet
+from .views import MovieViewSet, import_movies, ReviewViewSet, RatingViewset
 
 router = SimpleRouter()
 router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewset)
 
 
 movies_router = NestedSimpleRouter(router, 'movies', lookup='movie')
