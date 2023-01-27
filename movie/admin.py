7ca99efc772a2f4b25eb500cbaac1d50b0406495
MohamadrezaPiri <piry.mreza@gmail.com>
2023-01-27 12:26:29 +0330

    Add admin section

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Movie, Rating, Review
 
 # Register your models here.
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'release_date', 'cast', 'crew']
+    search_fields = ['title', 'cast']
+
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['movie', 'user', 'stars']
+    autocomplete_fields = ['user', 'movie']
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'movie', 'text']
+    autocomplete_fields = ['user', 'movie']
