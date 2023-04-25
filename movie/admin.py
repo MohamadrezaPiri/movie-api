d9617550d36740ec88a08b2d33f372fb3ec543a0
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-25 11:01:42 +0430

    Add reviews method in MovieAdmin

--- movie/admin.py
+++ movie/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import urlencode, format_html
 from .models import Movie, Rating, Review
 
 # Register your models here.
@@ -8,6 +10,15 @@ from .models import Movie, Rating, Review
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['title', 'release_date', 'cast', 'crew']
     search_fields = ['title', 'cast']
+
+    def reviews(self, movie):
+        url = (
+            reverse('admin:movie_movie_changelist')
+            + '?'
+            + urlencode({
+                'movie__id': str(movie.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, movie.reviews)
 
 
 @admin.register(Rating)
