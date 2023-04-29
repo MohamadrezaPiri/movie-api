4dea1bcc35a4c72db255cf274a36701c0e7e6d7a
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-29 10:33:42 +0430

    Add clear_votes to actions

--- movie/admin.py
+++ movie/admin.py
@@ -11,7 +11,7 @@ from .models import Movie, Rating, Review
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['title', 'release_date', 'cast', 'crew','reviews_count','votes','avg_rating']
     search_fields = ['title', 'cast']
-    actions = ['clear_reviews']
+    actions = ['clear_reviews', 'clear_votes']
 
     def reviews_count(self, movie):
         url = (
